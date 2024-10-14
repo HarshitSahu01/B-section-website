f884c6594c16c008578318b46e68cf067f4e64ba
Mohd. Shayan <sehbaqureshi@gmail.com>
2024-10-14 17:22:07 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx 	modified:   src/Components/Navbar.jsx 	modified:   src/Stylesheets/Navbar.module.css 	new file:   src/assets/logo.svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home'
-import About from './Pages/About'
-import Navbar from './Components/Navbar'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
-      <h1 className='text-center'>B section Website page</h1>
+      <h1 className="text-center">B section Website page</h1>
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/about' element={<About />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
       <footer>Footer hai ye</footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
