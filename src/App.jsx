28cd3143ab4ca901d04cb97cfa8fe71880c6de3e
HarshitSahu01 <sahuharshit2019@gmail.com>
2024-10-14 14:35:58 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx 	new file:   src/Pages/About.jsx 	new file:   src/Pages/Home.jsx 	new file:   src/Stylesheets/about.module.css 	modified:   src/main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
 
 function App() {
   return (
     <>
       <h1 className='text-center'>B section Website page</h1>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+      </Routes>
+      <footer>Footer hai ye</footer>
     </>
   )
 }
---
+++ src/Pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Style from '../Stylesheets/about.module.css'
+
+export default function About() {
+  return (
+    <div>
+      This will be the about page
+      <div className={Style.box}>Box</div>
+    </div>
+  )
+}
