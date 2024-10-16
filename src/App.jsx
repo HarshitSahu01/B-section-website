c37b95949ea40b6643a0fd72d368a62f26d0ccfe
Mohd. Shayan <sehbaqureshi@gmail.com>
2024-10-16 15:59:10 +0530

    modified:   package-lock.json 	modified:   src/App.jsx 	modified:   src/Pages/Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,14 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
-import Navbar from "./Components/Navbar";
-<<<<<<< HEAD
-import ParticleCursor from "./Components/Cursor";
-=======
 import Placeholder from "./Pages/Placeholder";
->>>>>>> ee66f4b9b9ebcd96384e57d10ab793670f8e3df2
-
+import Navbar from "./Components/Navbar";
+import ParticleCursor from "./Components/Cursor";
 function App() {
   return (
     <div id="app">
       <ParticleCursor />
       <Navbar />
-<<<<<<< HEAD
-      <div id="content">
-        <h1 className="text-center">B Section Website Page</h1>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-      <footer>Footer hai ye</footer>
-    </div>
-=======
       {/* <h1 className="text-center">B section Website page</h1> */}
       <Routes>
         <Route path="/" element={<Placeholder />} />
@@ -32,8 +17,7 @@ function App() {
         <Route path="/about" element={<About />} />
       </Routes>
       {/* <footer>Footer hai ye</footer> */}
-    </>
->>>>>>> ee66f4b9b9ebcd96384e57d10ab793670f8e3df2
+    </div>
   );
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,22 +1,13 @@
-<<<<<<< HEAD
-import React from 'react';
 
-=======
 import React from 'react'
 import HomeAbout from '../Components/HomeAbout'
 import GroupImage from '../Components/GroupImage'
->>>>>>> ee66f4b9b9ebcd96384e57d10ab793670f8e3df2
 
 export default function Home() {
   return (
     <div>
-<<<<<<< HEAD
-      <h1>Welcome to the Main Page</h1>
-      
-=======
       <GroupImage />
       <HomeAbout/>
->>>>>>> ee66f4b9b9ebcd96384e57d10ab793670f8e3df2
     </div>
   );
 }
