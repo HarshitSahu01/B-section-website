dee1c198ebafb50e91ac680cd86945f22f6d4e75
Mohd. Shayan <sehbaqureshi@gmail.com>
2024-10-17 14:30:20 +0530

    Shayan's Commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,23 @@ import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Placeholder from "./Pages/Placeholder";
 import Navbar from "./Components/Navbar";
-import ParticleCursor from "./Components/Cursor";
+import CursorTrail from "./Components/CursorTrail";
+
 function App() {
   return (
-    <div id="app">
-      <ParticleCursor />
+    <div>
+      
       <Navbar />
-      {/* <h1 className="text-center">B section Website page</h1> */}
-      <Routes>
-        <Route path="/" element={<Placeholder />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-      {/* <footer>Footer hai ye</footer> */}
+      <CursorTrail/>
+      <div className="min-h-screen">
+        <h1 className="text-center">B Section Website Page</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, esse! Aspernatur, quasi! Aperiam eligendi voluptate id laboriosam quaerat quo a perspiciatis, totam tempora deserunt, nisi, maxime nihil quam voluptatibus officia expedita! Nemo iure suscipit omnis laboriosam molestias. Nulla perferendis dolor necessitatibus culpa sint iure ex at sed placeat, mollitia praesentium quia atque quam debitis quae facere. Minus quas nesciunt perferendis. Facilis, quod neque dolorum pariatur id totam ab aspernatur. Illo laborum quaerat sed autem blanditiis minima dolore fugit quasi, magni velit sunt similique, aspernatur earum aut eius nobis repudiandae cum rem dolores! Sapiente in, excepturi cumque eos saepe omnis modi fuga blanditiis voluptas. Nihil, modi! Velit aut libero accusantium ab expedita autem exercitationem cumque amet sunt cum corrupti dolore distinctio consectetur, quia tenetur? Pariatur nulla, nostrum, eius placeat asperiores facere eveniet delectus aliquid labore possimus molestiae a reiciendis, maxime quam ullam omnis nisi. Laborum dignissimos, eius doloremque impedit optio rerum iusto ea architecto! Asperiores dicta eum rem velit esse odit neque blanditiis? Nam, expedita cupiditate, sed tempora necessitatibus omnis iste hic eveniet fuga itaque doloremque voluptate nobis distinctio inventore quibusdam accusamus esse voluptates magni quas molestias ducimus at sequi cumque! Praesentium earum beatae modi dignissimos, accusamus distinctio at nulla sequi, voluptatum placeat alias nostrum rerum ducimus quos quod, eos maiores ea. Enim nam rerum quae laborum ea, consequuntur corrupti beatae asperiores cum adipisci perferendis. Pariatur, accusantium nemo eos nulla quibusdam dignissimos distinctio aliquam, molestiae ducimus tempora deserunt. Maiores animi consequuntur suscipit ad? Alias eligendi vel libero numquam reiciendis doloribus voluptates quasi, odio odit dignissimos dicta praesentium debitis explicabo incidunt perspiciatis exercitationem beatae reprehenderit nostrum tempore sapiente quam sed inventore repellendus accusantium. Similique nesciunt modi repellendus autem quasi vel omnis harum alias id, dolorum quia quis quae quas porro, sunt, nobis incidunt vero quidem exercitationem maxime dolorem totam molestias laboriosam eligendi. Quisquam veniam quas consectetur voluptate, quo officiis laborum illum alias blanditiis doloremque est temporibus numquam voluptas repudiandae at esse ex vitae laboriosam non voluptates assumenda dicta impedit! Possimus, consequuntur fugiat eveniet similique cumque cum eum iure hic ipsam iusto esse laboriosam ipsum repellendus unde est quaerat dignissimos nostrum impedit accusamus. Odio, obcaecati odit! Blanditiis placeat nostrum explicabo, quis ratione molestias recusandae minus totam excepturi laborum repudiandae ex ut magni officiis iusto, sint, nihil necessitatibus quo libero tempore asperiores dignissimos at debitis natus. Quia, voluptatum assumenda qui beatae omnis cupiditate! Repellat sequi itaque obcaecati doloribus nihil vel, rem perspiciatis nulla debitis cumque, necessitatibus magni laborum incidunt, beatae accusamus temporibus error fuga recusandae eos exercitationem quaerat et. Consequuntur, quo quod atque eveniet asperiores nemo quibusdam quisquam ipsam adipisci dolores natus iure corporis. Laudantium voluptatibus facere laboriosam autem corrupti fuga mollitia, vel amet magni asperiores laborum repellat magnam ipsa illo dolor delectus adipisci ea distinctio odio! Ab nemo animi doloremque id quae autem, illo maxime assumenda? Soluta ipsum ea repellendus quae voluptates? Accusantium reprehenderit, exercitationem iure sit facere, magnam, ut officia explicabo hic nulla assumenda cumque voluptatibus soluta porro id architecto mollitia! Quasi officia blanditiis temporibus praesentium, aut repellendus tempore voluptatum debitis laborum.</p>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+      <footer className="bg-opacity-80 bg-slate-500 h-[10vh] flex justify-center items-center">Footer hai ye</footer>
     </div>
   );
 }
---
+++ src/Components/CursorTrail.jsx
@@ -0,0 +1,109 @@
+// CursorTrail.jsx
+import React, { useEffect } from 'react';
+import styles from '../Stylesheets/Cursor.module.css';
+
+const CursorTrail = () => {
+  useEffect(() => {
+    const cursorTailAnimation = document.querySelector('#cursor-tail-animation');
+    const coords = { x: 0, y: 0 };
+
+    for (let i = 0; i < 25; i++) {
+      const circle = document.createElement('div');
+      circle.classList.add(styles.circle);
+      cursorTailAnimation.appendChild(circle);
+    }
+
+    const circles = document.querySelectorAll(`.${styles.circle}`);
+
+    // CHOOSE COLOR
+    const colors = [];
+    for (let i = 0; i < 25; i++) {
+      colors.push(`hsl(${i + 15}, 100%, ${i % 2 === 0 ? 50 : 70}%)`);
+    }
+
+    circles.forEach(function (circle, index) {
+      circle.x = 0;
+      circle.y = 0;
+      circle.style.backgroundColor = colors[index % colors.length];
+    });
+
+    const handleMouseMove = (e) => {
+      coords.x = e.clientX;
+      coords.y = e.clientY;
+    };
+
+    const animateCircles = () => {
+      let x = coords.x;
+      let y = coords.y;
+
+      circles.forEach(function (circle, index) {
+        circle.style.left = x - 12 + 'px';
+        circle.style.top = y - 12 + 'px';
+
+        circle.style.scale = (circles.length - index) / circles.length;
+
+        circle.x = x;
+        circle.y = y;
+
+        const nextCircle = circles[index + 1] || circles[0];
+        x += (nextCircle.x - x) * 0.3;
+        y += (nextCircle.y - y) * 0.3;
+      });
+
+      requestAnimationFrame(animateCircles);
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    animateCircles();
+
+    window.addEventListener("click", createBlast);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+      // Clean up circles
+      cursorTailAnimation.innerHTML = '';
+    };
+  }, []);
+
+  function createBlast(e) {
+    const blastCount = 30; // Number of particles
+    const particles = [];
+
+    for (let i = 0; i < blastCount; i++) {
+      const particle = document.createElement("div");
+      particle.classList.add(styles.particle);
+      particle.style.left = `${e.clientX}px`;
+      particle.style.top = `${e.clientY}px`;
+      document.body.appendChild(particle);
+      particles.push(particle);
+
+      // Randomize the direction and speed of each particle
+      const angle = Math.random() * 2 * Math.PI;
+      const speed = Math.random() * 5 + 2;
+      const xSpeed = Math.cos(angle) * speed;
+      const ySpeed = Math.sin(angle) * speed;
+
+      // Animate each particle
+      setTimeout(() => {
+        particle.style.transform = `translate(${xSpeed * 20}px, ${
+          ySpeed * 20
+        }px) scale(0)`;
+        particle.style.opacity = "0";
+      }, 0);
+
+      // Remove particle after animation
+      setTimeout(() => {
+        document.body.removeChild(particle);
+      }, 1000);
+    }
+  }
+
+
+  return (
+    <div className={styles.cursorTailAnimation} id="cursor-tail-animation">
+      <h1 className={styles.mobileText} id="mobile-text"></h1>
+    </div>
+  );
+};
+
+export default CursorTrail;
